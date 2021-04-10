ba063bf76c9f19b7347162bff71c63190681bdd6
Numulix <jbabic999@gmail.com>
2021-04-10 12:59:30 +0200

    Added necessary map dependencies

--- AlertWatch/app/src/main/java/raf/rs/alertwatch/MainActivity.java
+++ AlertWatch/app/src/main/java/raf/rs/alertwatch/MainActivity.java
@@ -1,14 +1,49 @@
 package raf.rs.alertwatch;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
 public class MainActivity extends AppCompatActivity {
+
+    SupportMapFragment supportMapFragment;
+    FusedLocationProviderClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.maps_fragment);
+
+        client = LocationServices.getFusedLocationProviderClient(this);
+
+        // Provera permisija
+        if (ActivityCompat.checkSelfPermission(MainActivity.this,
+                Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            // PERMISSION GRANTED -> Call method
+            getCurrentLocation();
+        }
+    }
+
+    private void getCurrentLocation() {
+        Task<Location> task = client.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+
+            }
+        });
     }
 }
