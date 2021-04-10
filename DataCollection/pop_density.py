daadd3272b19468c562721a0cef06a380a075af7
FreeJ99 <jenkoslobodan@gmail.com>
2021-04-10 21:54:22 +0200

    "Created an optimizer for resource allocation."

--- DataCollection/pop_density.py
+++ DataCollection/pop_density.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
-from skimage.transform import rescale
 import numpy as np
+import pandas as pd
 
+plt.rcParams["figure.figsize"] = (15, 15)
 def load_population_matrix(path, scale):
     # Locations of Japans map corners:
     # Upper Left  ( 122.9320829,  45.5245833) (122d55'55.50"E, 45d31'28.50"N)
@@ -14,6 +15,46 @@ def load_population_matrix(path, scale):
     img = plt.imread(path)
     img = img[:,:,0]
     return rescale(img, scale, anti_aliasing=False)
+
+def get_optimal_distribution(available_res=10000):
+    cities = pd.read_csv('jp.csv')[['city', 'population']]
+    goal = np.random.randint(10, 100, size=len(cities))
+    n = len(cities)
+    cities.head()
+
+    def cost_fun(x):
+    return np.mean((x-goal)**2)
+
+    def constraint1(x):
+        return available_res - np.sum(x)
+
+    # initial guesses
+    x0 = np.zeros(n)
+    x0[0] = available_res
+
+    # show initial objective
+    # print('Initial Objective: ' + str(cost_fun(x0)))
+
+    # optimize
+    b = (0, available_res)
+    bnds = [b for _ in range(n)] # np.tile(b, (1, len(cities)))
+
+    con1 = {'type': 'ineq', 'fun': constraint1}
+    cons = ([con1])
+    solution = minimize(cost_fun, x0, bounds=bnds, constraints=cons)
+    x = np.array(solution.x).astype(np.int64)
+
+
+    # show final objective
+    # print('Final Objective: ' + str(cost_fun(x)))
+
+    # print('Used resources:', sum(x))
+
+    # print solution
+    # print('Solution')
+    # print(x)
+
+    return [x for x in zip(cities['city'].values, x)]
 
 if __name__ == "__main__":
     img = load_population_matrix('jpn_ppp_2020_1km_Aggregated.tif', 0.2)
