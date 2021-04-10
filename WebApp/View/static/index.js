3b7ad88996a8d4585433f0717c1b2232807632d2
Numulix <jbabic999@gmail.com>
2021-04-10 20:16:00 +0200

    Marked locations on recent earthquakes based on fetched data

--- WebApp/View/static/index.js
+++ WebApp/View/static/index.js
@@ -8,4 +8,25 @@ function initMap() {
     }
 
     map = new google.maps.Map(document.getElementById("map"), options);
+    hourlyEq();
+}
+
+function hourlyEq() {
+    fetch("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson")
+        .then(response => response.json())
+        .then(data => {
+            features = data.features;
+
+            for (let i = 0; i < features.length; i++) {
+                let lat = features[i].geometry.coordinates[1];
+                let lng = features[i].geometry.coordinates[0];
+                let latLng = new google.maps.LatLng(lat, lng);
+
+                new google.maps.Marker({
+                    position: latLng,
+                    map: map
+                });
+            }
+
+        });
 }
