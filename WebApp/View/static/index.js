e8a54aabc9434562055425a329041c1e61d90a50
vmatovic <vmatovic3818rn@raf.rs>
2021-04-10 20:44:04 +0200

    Mapa zemljotresa gde su epicentri oznaceni krugovima

--- WebApp/View/static/index.js
+++ WebApp/View/static/index.js
@@ -9,6 +9,13 @@ function initMap() {
 
     map = new google.maps.Map(document.getElementById("map"), options);
     hourlyEq();
+	
+	  map.data.setStyle(function(feature) {
+		var magnitude = feature.getProperty('mag');
+		return {
+		  icon: getCircle(magnitude)
+		};
+	  });
 }
 
 function hourlyEq() {
@@ -21,11 +28,18 @@ function hourlyEq() {
                 let lat = features[i].geometry.coordinates[1];
                 let lng = features[i].geometry.coordinates[0];
                 let latLng = new google.maps.LatLng(lat, lng);
+				let magnitude = features[i].properties.mag;
 
-                new google.maps.Marker({
-                    position: latLng,
-                    map: map
-                });
+                const circle = new google.maps.Circle({
+				  strokeColor: "#FF0000",
+				  strokeOpacity: 0.8,
+				  strokeWeight: 2,
+				  fillColor: "#FF0000",
+				  fillOpacity: 0.35,
+				  map,
+				  center: latLng,
+				  radius: Math.pow(2, magnitude)/2,
+				});
             }
 
         });
