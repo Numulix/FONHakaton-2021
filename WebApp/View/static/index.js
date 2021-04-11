ad8f202e5e47ccb3f6f2e08f03f9b729bebbccba
Numulix <jbabic999@gmail.com>
2021-04-11 13:36:42 +0200

    Visualized epicenters from data with estimated radius on the map

--- WebApp/View/static/index.js
+++ WebApp/View/static/index.js
@@ -1,13 +1,41 @@
 var map;
+const circles = [];
+
+var dummy_epicenters = [
+    {
+        epicenter: [44.117512, 20.189369],
+        magnitude: 6
+    }, {
+        epicenter: [43.886935, 20.556935],
+        magnitude: 7
+    }, {
+        epicenter: [43.654546, 20.838094],
+        magnitude: 7
+    }]
 
 function initMap() {
 
     let options = {
         center: { lat: 44.787197, lng: 20.457273 },
-        zoom: 8
+        zoom: 7
     }
 
     map = new google.maps.Map(document.getElementById("map"), options);
+
+    for (let i = 0; i < dummy_epicenters.length; i++) {
+        let latLng = new google.maps.LatLng(dummy_epicenters[i].epicenter[0], dummy_epicenters[i].epicenter[1])
+        circles.push(new google.maps.Circle({
+            strokeColor: "#FF0000",
+			strokeOpacity: 0.8,
+			strokeWeight: 2,
+			fillColor: "#FF0000",
+			fillOpacity: 0.35,
+			map,
+            center: latLng,
+            radius: Math.pow(2, dummy_epicenters[i].magnitude) * 100
+        }))
+    }
+
     hourlyEq();
 	
 	  map.data.setStyle(function(feature) {
@@ -19,7 +47,7 @@ function initMap() {
 }
 
 function hourlyEq() {
-    fetch("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson")
+    fetch("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson")
         .then(response => response.json())
         .then(data => {
             features = data.features;
@@ -65,4 +93,15 @@ function hourlyEq() {
             })
 
         });
+}
+
+function addMarker() {
+    let lat = document.getElementById("latitude").value
+    let lng = document.getElementById("longitude").value
+
+    let latLng = new google.maps.LatLng(lat, lng)
+    new google.maps.Marker({
+        position: latLng,
+        map: map
+    })
 }
