c333a22c1657726de83b5845ec396a2817214a2e
vmatovic <vmatovic3818rn@raf.rs>
2021-04-11 16:30:39 +0200

    Tabela optimalne raspodele

--- WebApp/View/views.py
+++ WebApp/View/views.py
@@ -3,10 +3,13 @@ import sys
 import os
 from View import DataCollection
 from View.DataCollection import pop_density
-import simplejson
 from . import voronoi
+import simplejson
 import io
 import urllib, base64
+import os.path
+import json
+BASE = os.path.dirname(os.path.abspath(__file__))
 
 # os.path.join(os.path.dirname(os.path.dirname(__file__)), 'DataCollection', 'jp.csv')
 
@@ -14,12 +17,14 @@ import urllib, base64
 
 def main_page(request):
 	opt = pop_density.get_optimal_distribution()
+	f = open(os.path.join(BASE, "json_gradovi.json"), )
+	data = json.load(f)
 	ll1=[]
 	for i, x in enumerate(opt):
 		ll1.append(str(x[0]))
 		ll1.append(str(x[1]))
 	json_list = simplejson.dumps(ll1)
-	return render(request, 'home.html', {'data2': json_list})
+	return render(request, 'home.html', {'data2': json_list, 'tabela': data})
 
 
 def voronoi_prikaz(request):
